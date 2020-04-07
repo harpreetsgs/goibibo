cfa1e3946051566d133fa28dfb990ebdc0124aa5
harpreetsgs <harpreet.singh.cloud@gmail.com>
2020-04-07 15:39:07 +0530

    third commit

--- new/src/test/java/mproject/Ibibo.java
+++ new/src/test/java/mproject/Ibibo.java
@@ -1,5 +1,6 @@
 package mproject;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
@@ -7,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.browser.Browser;
 
 public class Ibibo {
 	
@@ -25,32 +27,28 @@ public static String nights ="//div[contains(@class,'NightCountWrap-fity7j')]";
 	public static String city="//input[contains(@class,'HomePageAutosuggeststyles')]";
 	public static String submit="//button[contains(@class,'SearchButtonAutoSuggest')]";
 	 public static String any="(//span[contains(@class,'dqxIKI')])[3]";
-	 public static String hotelname="(//span[contains(@class,'HotelNameTextSpan-sc')])[1]";
-	public static String hotelprice="(//span[contains(@class,'CurrentPrice-sc')])[1]";
-		public static String ratings="(//span[contains(@class,'ReviewAndRatingsstyles')])[2]";
+	 
+	 
+	 public static String hotelname="(//span[contains(@class,'HotelNameTextSpan')])[2]";
+	public static String hotelprice="(//span[contains(@class,'CurrentPrice-sc')])[2]";
+	public static String ratings="(//span[contains(@class,'ReviewAndRatingsstyles__AverageReviewText')])[2]";
 
-	public static String selectroom ="(//button[contains(@class,'RoomFlavor__ButtonWrapper-guj4pt-18 gQCOQU'])[8]";
-	public static String sroom="(//button[contains(@class,'RoomFlavor')])[1]";
-	public static String name="(//input[(@type='text')])[1]";
-	public static String mname="(//input[(@type='text')])[2]";
-	public static String email ="(//input[(@type='text')])[3]";
-	public static String num="(//input[(@type='text')])[4]";
-
+	
 	public static String Submit1="(//button[contains(@class,'GuestDetailBlock')])[1]";
 	
 
 	
 	public static void main(String[] args) throws Exception
 	{ 
-		System.setProperty("webdriver.chrome.driver","H:\\ch\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","H:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();	
+		driver.manage().window().maximize();
 		driver.get(" https://www.goibibo.com");
 		driver.findElement(By.xpath(bus)).click();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath(date1)).click();
 		driver.findElement(By.xpath(nextmonth)).click();
 		driver.findElement(By.xpath(nextmonth)).click();
-		//driver.findElement(By.xpath(nextmonth)).click();
 
 		driver.findElement(By.xpath(date)).click();
 		Thread.sleep(2000);
@@ -63,53 +61,52 @@ public static String nights ="//div[contains(@class,'NightCountWrap-fity7j')]";
 		driver.findElement(By.xpath(rooms)).click();
 		driver.findElement(By.xpath(add)).click();
 		driver.findElement(By.xpath(done)).click();
-		//driver.findElement(By.xpath(city)).click();;
 		WebElement city1= driver.findElement(By.xpath(city));
 		city1.sendKeys("Pune");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath(any)).click();
 		driver.findElement(By.xpath(submit)).click();
 
-		File newTextFile = new File("H:/ibibo.txt");
+		File newTextFile = new File("H://ibibo.txt");
 
         FileWriter fw = new FileWriter(newTextFile);
-        
-		for (int i = 0; i < 3; i++)
-	    {
-	    	WebElement p=driver.findElement(By.xpath(hotelname.replace("replace"," ' +i '")));
-	    	WebElement q=driver.findElement(By.xpath(hotelprice.replace("replace"," ' +i '")));
-	    	WebElement r=driver.findElement(By.xpath(ratings.replace("replace"," '+i '")));
-	    	
+        BufferedWriter bw = new BufferedWriter(fw);
+        synchronized (driver) {
+			
+		
+		for (int i = 1; i < 5; i++)
+	    {	    	driver.wait(5000);
+
+	    	WebElement p=driver.findElement(By.xpath("(//span[contains(@class,'HotelName')])["+ i + "]"));
+	    	driver.wait(5000);
+	    	WebElement q=driver.findElement(By.xpath("(//span[contains(@class,'CurrentPrice')])["+ i + "]"));
+	    	driver.wait(5000);
+
+	    	WebElement r= driver.findElement(By.xpath("(//span[contains(@class,'AverageReviewText')])["+ i + "]"));
+	    	driver.wait(5000);
+
 	    	String mname=p.getText();
 	    	System.out.println(mname);
+	    	
 	    	String mprice=q.getText();
 	    	System.out.println(mprice);
 
 	    	String mratings=r.getText();
 	    	System.out.println(mratings);
-	    
-	    	fw.write(mname);
-	    	fw.write(mprice);
-	    	fw.write(mratings);
-
 	    	
-
-	    }
-        fw.close();
-
+	    	bw.write(mname);
+	    	bw.write(mprice);
+	    	bw.write(mratings);
+	    
+	    	
+	    }}
+		bw.close();
+		fw.close();
+		
+		
 		
 
-		driver.findElement(By.xpath(selectroom)).click();
-
-		driver.findElement(By.xpath(sroom)).click();
-		driver.findElement(By.xpath(name)).sendKeys("harry");
-
-		driver.findElement(By.xpath(mname)).sendKeys("Singh");
-		driver.findElement(By.xpath(email)).sendKeys("harryxperias@gmail.com");
-		driver.findElement(By.xpath(num)).sendKeys("942210033");
-		driver.findElement(By.xpath(submit)).click();
-
-		
+		driver.quit();
 		
 	}
 
